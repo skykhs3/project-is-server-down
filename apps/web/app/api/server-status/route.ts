833064cca4c9b3ee8e6d6d4ff0f0fd76a9f20138
skykhs3 <skykhs3@naver.com>
2025-04-28 19:59:44 +0900

    feat: api connect

--- apps/web/app/api/server-status/route.ts
+++ apps/web/app/api/server-status/route.ts
@@ -1,91 +1,10 @@
-import clientPromise from "@/lib/mongodb";
-import { AllServerStatus, DomainStatus, ServerStatus } from "@/page";
+import { AllServerStatus } from "@/page";
 
 export async function GET(): Promise<Response> {
   try {
-    const client = await clientPromise;
-    const db = client.db("project-is-server-down");
-    const collection = db.collection("website_checks");
-
-    const data = await collection
-      .aggregate([
-        { $sort: { "metadata.name": 1, timestamp: -1 } },
-        {
-          $group: {
-            _id: "$metadata.name",
-            items: { $push: "$$ROOT" },
-          },
-        },
-        {
-          $project: {
-            _id: 1,
-            items: { $slice: ["$items", 60] },
-          },
-        },
-        {
-          $lookup: {
-            from: "website_checks",
-            let: { name: "$_id" },
-            pipeline: [
-              {
-                $match: {
-                  $expr: {
-                    $and: [
-                      { $eq: ["$metadata.name", "$$name"] },
-                      { $eq: ["$metadata.statusCode", 0] },
-                    ],
-                  },
-                },
-              },
-              { $sort: { timestamp: -1 } },
-              { $limit: 1 },
-              { $project: { timestamp: 1, _id: 0 } },
-            ],
-            as: "latestStatusZero",
-          },
-        },
-        {
-          $addFields: {
-            latestStatusZeroTimestamp: {
-              $ifNull: [
-                { $arrayElemAt: ["$latestStatusZero.timestamp", 0] },
-                null,
-              ],
-            },
-          },
-        },
-        {
-          $project: {
-            latestStatusZero: 0,
-          },
-        },
-      ])
-      .toArray();
-
-    const res: AllServerStatus = {};
-    for (const item of data) {
-      const name = item._id;
-      const lastDowntime: string = item.latestStatusZeroTimestamp;
-      const history: ServerStatus[] = item.items.map((ele: any) => {
-        return {
-          timestamp: ele.timestamp,
-          responseTimeMs:
-            ele.metadata.statusCode === 0 ? 0 : ele.responseTimeMs,
-        };
-      });
-      const isOnline: boolean = item.items[0].metadata.statusCode !== 0;
-      const url: string = item.items[0].metadata.url;
-
-      const domainStatus: DomainStatus = {
-        lastDowntime,
-        isOnline,
-        history,
-        url,
-      };
-      res[name] = domainStatus;
-    }
-
-    return Response.json(res satisfies AllServerStatus);
+    const res = await fetch("http://localhost:9999/api/server-status");
+    const data = await res.json();
+    return Response.json(data satisfies AllServerStatus);
   } catch (error) {
     return Response.json({} satisfies AllServerStatus);
   }
--- apps/web/app/lib/mongodb.ts
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from "mongodb";
-
-const uri = process.env.MONGODB_URI || "mongodb://localhost:27017";
-const options = {};
-
-const client = new MongoClient(uri, options);
-const clientPromise = client.connect();
-
-export default clientPromise;
