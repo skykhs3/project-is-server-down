ebf229e5c0a1f5ca4f72b23bf51127f3fcb16667
skykhs3 <skykhs3@naver.com>
2025-04-30 04:27:31 +0900

    test: reuse mongoclient

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -10,8 +10,16 @@ if (!uri) {
 }
 
 const options = {};
-const client = new MongoClient(uri, options);
-const clientPromise = client.connect();
+let client: MongoClient | null = null;
+
+async function getMongoClient() {
+  if (!client) {
+    client = new MongoClient(uri as string, options);
+    await client.connect();
+  }
+  return client;
+}
+
 const app = express();
 
 export type AllServerStatus = Record<string, DomainStatus>;
@@ -27,95 +35,109 @@ export interface DomainStatus {
   url: string;
 }
 
+interface SavedResult {
+  result: AllServerStatus;
+  millisecondsTimestamp: number;
+}
+
+const savedResult: SavedResult = {
+  result: {},
+  millisecondsTimestamp: 0,
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/api/server-status", async (req: Request, res: Response) => {
   try {
-    const client = await clientPromise;
-    const db = client.db("project-is-server-down");
-    const collection = db.collection("website_checks");
-
-    const data = await collection
-      .aggregate([
-        { $sort: { "metadata.name": 1, timestamp: -1 } },
-        {
-          $group: {
-            _id: "$metadata.name",
-            items: { $push: "$$ROOT" },
-          },
-        },
-        {
-          $project: {
-            _id: 1,
-            items: { $slice: ["$items", 60] },
-          },
-        },
-        {
-          $lookup: {
-            from: "website_checks",
-            let: { name: "$_id" },
-            pipeline: [
-              {
-                $match: {
-                  $expr: {
-                    $and: [
-                      { $eq: ["$metadata.name", "$$name"] },
-                      { $eq: ["$metadata.statusCode", 0] },
-                    ],
-                  },
-                },
-              },
-              { $sort: { timestamp: -1 } },
-              { $limit: 1 },
-              { $project: { timestamp: 1, _id: 0 } },
-            ],
-            as: "latestStatusZero",
-          },
-        },
-        {
-          $addFields: {
-            latestStatusZeroTimestamp: {
-              $ifNull: [
-                { $arrayElemAt: ["$latestStatusZero.timestamp", 0] },
-                null,
-              ],
+    if (savedResult.millisecondsTimestamp + 1000 < Date.now()) {
+      const client = await getMongoClient();
+      const db = client.db("project-is-server-down");
+      const collection = db.collection("website_checks");
+      const data = await collection
+        .aggregate([
+          { $sort: { "metadata.name": 1, timestamp: -1 } },
+          {
+            $group: {
+              _id: "$metadata.name",
+              items: { $push: "$$ROOT" },
             },
           },
-        },
-        {
-          $project: {
-            latestStatusZero: 0,
+          {
+            $project: {
+              _id: 1,
+              items: { $slice: ["$items", 60] },
+            },
           },
-        },
-      ])
-      .toArray();
+          {
+            $lookup: {
+              from: "website_checks",
+              let: { name: "$_id" },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $and: [
+                        { $eq: ["$metadata.name", "$$name"] },
+                        { $eq: ["$metadata.statusCode", 0] },
+                      ],
+                    },
+                  },
+                },
+                { $sort: { timestamp: -1 } },
+                { $limit: 1 },
+                { $project: { timestamp: 1, _id: 0 } },
+              ],
+              as: "latestStatusZero",
+            },
+          },
+          {
+            $addFields: {
+              latestStatusZeroTimestamp: {
+                $ifNull: [
+                  { $arrayElemAt: ["$latestStatusZero.timestamp", 0] },
+                  null,
+                ],
+              },
+            },
+          },
+          {
+            $project: {
+              latestStatusZero: 0,
+            },
+          },
+        ])
+        .toArray();
 
-    const result: AllServerStatus = {};
-    for (const item of data) {
-      const name = item._id;
-      const lastDowntime: string = item.latestStatusZeroTimestamp;
-      const history: ServerStatus[] = item.items.map((ele: any) => {
-        return {
-          timestamp: ele.timestamp,
-          responseTimeMs:
-            ele.metadata.statusCode === 0 ? 0 : ele.responseTimeMs,
+      const result: AllServerStatus = {};
+      for (const item of data) {
+        const name = item._id;
+        const lastDowntime: string = item.latestStatusZeroTimestamp;
+        const history: ServerStatus[] = item.items.map((ele: any) => {
+          return {
+            timestamp: ele.timestamp,
+            responseTimeMs:
+              ele.metadata.statusCode === 0 ? 0 : ele.responseTimeMs,
+          };
+        });
+        const isOnline: boolean = item.items[0].metadata.statusCode !== 0;
+        const url: string = item.items[0].metadata.url;
+
+        const domainStatus: DomainStatus = {
+          lastDowntime,
+          isOnline,
+          history,
+          url,
         };
-      });
-      const isOnline: boolean = item.items[0].metadata.statusCode !== 0;
-      const url: string = item.items[0].metadata.url;
+        result[name] = domainStatus;
+      }
 
-      const domainStatus: DomainStatus = {
-        lastDowntime,
-        isOnline,
-        history,
-        url,
-      };
-      result[name] = domainStatus;
+      savedResult.result = result;
+      // savedResult.millisecondsTimestamp = Date.now();
     }
 
-    res.status(200).json(result);
+    res.status(200).json(savedResult.result);
   } catch (error) {
     res.status(200).json({});
   }
